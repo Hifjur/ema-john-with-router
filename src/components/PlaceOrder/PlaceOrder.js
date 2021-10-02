1dfbfda3eb07d3bdba8220808fe3618dbb93234a
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-10-03 03:59:46 +0600

    complete

---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import img from '../../images/giphy.gif'
+const PlaceOrder = () => {
+    return (
+        <div>
+            <img src={img}></img>
+        </div>
+    );
+};
+
+export default PlaceOrder;
